a3729438fd86e47661f6c35e3953b834c39b2045
lukasabotic <luka.sabotic@gmail.com>
2022-05-08 19:57:16 +0200

    minor changes to homescreen

--- app/lib/src/features/home/presentation/widgets/neon_sign/neon_text.dart
+++ app/lib/src/features/home/presentation/widgets/neon_sign/neon_text.dart
@@ -1,6 +1,5 @@
 import 'package:app/src/config/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class NeonText extends StatelessWidget {
   const NeonText({Key? key, required this.text}) : super(key: key);
